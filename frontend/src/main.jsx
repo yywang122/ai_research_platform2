60bf5bf4470001ce3bd61ca1b4ad74bbcaae2563
yywang122 <sophia880122@gmail.com>
2026-03-16 13:06:58 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import App from './App.jsx'
+import { theme } from './theme/theme'
+import { AuthProvider } from './contexts/AuthContext'
+import { PaperSearchProvider } from './contexts/PaperSearchContext'
+import { LibraryProvider } from './contexts/LibraryContext'
+import { AnalysisProvider } from './contexts/AnalysisContext'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <AuthProvider>
+          <PaperSearchProvider>
+            <LibraryProvider>
+              <AnalysisProvider>
+                <App />
+              </AnalysisProvider>
+            </LibraryProvider>
+          </PaperSearchProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
